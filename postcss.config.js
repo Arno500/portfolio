2f97b91882c0635cd249743042afafb547390284
Arno DUBOIS <arno.du@orange.fr>
2019-02-25 22:58:31 +0100

    Irritating

--- postcss.config.js
+++ postcss.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   plugins: {
     "postcss-preset-env": {},
-    "postcss-uncss": {
-      html: ["index.html"],
-      ignore: [
-        ":hover",
-        ":focus",
-        ":before",
-        ":after",
-        "figure",
-        "picture",
-        "h1",
-        "h2",
-        "p",
-        "div",
-        "body",
-        "html",
-        "*",
-        /(.grid).*/gm
-      ]
-    },
+    // "postcss-uncss": {
+    //   html: ["index.html"],
+    //   ignore: [
+    //     ":hover",
+    //     ":focus",
+    //     ":before",
+    //     ":after",
+    //     "figure",
+    //     "picture",
+    //     "h1",
+    //     "h2",
+    //     "p",
+    //     "div",
+    //     "body",
+    //     "html",
+    //     "*",
+    //     /(.grid).*/gm
+    //   ]
+    // },
     cssnano: {}
   }
 };
